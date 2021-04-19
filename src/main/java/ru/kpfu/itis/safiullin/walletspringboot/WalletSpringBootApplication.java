d4f81d7d2c8ec0ea3fb1a2beb9d12747cb92e387
Ruslan Safiullin <rus.safiullin.01@mail.ru>
2021-04-19 23:40:50 +0300

    init

---
+++ src/main/java/ru/kpfu/itis/safiullin/walletspringboot/WalletSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.safiullin.walletspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSpringBootApplication.class, args);
+    }
+
+}
