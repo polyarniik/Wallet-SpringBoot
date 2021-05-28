55342d27321829cc1323ce35236550bd30cf5a4c
Ruslan Safiullin <rus.safiullin.01@mail.ru>
2021-05-28 07:06:12 +0300

    + add bank

--- src/main/java/ru/kpfu/itis/safiullin/walletspringboot/controllers/BankController.java
+++ src/main/java/ru/kpfu/itis/safiullin/walletspringboot/controllers/BankController.java
@@ -1,13 +1,14 @@
 package ru.kpfu.itis.safiullin.walletspringboot.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kpfu.itis.safiullin.walletspringboot.dto.RecordDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+import ru.kpfu.itis.safiullin.walletspringboot.dto.BankDto;
 import ru.kpfu.itis.safiullin.walletspringboot.forms.BankForm;
-import ru.kpfu.itis.safiullin.walletspringboot.models.Bank;
 import ru.kpfu.itis.safiullin.walletspringboot.security.details.AccountDetailsImpl;
 import ru.kpfu.itis.safiullin.walletspringboot.services.BankService;
 
@@ -21,8 +22,25 @@ public class BankController {
     }
 
     @GetMapping("/addBank")
-    public String getWalletPage(Model model, @AuthenticationPrincipal AccountDetailsImpl accountDetails) {
+    public String getBankPage(Model model, @AuthenticationPrincipal AccountDetailsImpl accountDetails) {
         model.addAttribute("form", new BankForm());
         return "views/add_bank";
+    }
+
+    @PostMapping("/addBank")
+    public RedirectView postBank(@AuthenticationPrincipal AccountDetailsImpl accountDetails,
+                                 BankForm bankForm, BindingResult bindingResult, Model model) {
+        if (!bindingResult.hasErrors()) {
+            String color = "#F0F0F0";
+            bankService.addBankToUser(BankDto.builder()
+                            .name(bankForm.getName())
+                            .color(color)
+                            .amount(bankForm.getAmount())
+                            .build(),
+                    accountDetails.getId());
+            return new RedirectView("/");
+        } else {
+            return new RedirectView("/addBank");
+        }
     }
 }
